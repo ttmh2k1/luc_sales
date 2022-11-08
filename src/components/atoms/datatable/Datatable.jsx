74c9fd8235ff708f1888f25b6229f52beddc0dfc
unknown <tranthimyhuyen10@gmail.com>
2022-11-09 02:58:45 +0700

    done Setting group

--- src/components/atoms/datatable/Datatable.jsx
+++ src/components/atoms/datatable/Datatable.jsx
@@ -1,50 +1,50 @@
-import "./datatable.scss";
-import { DataGrid } from "@mui/x-data-grid";
-import { userColumns, userRows } from "../../../dataTableSource";
-import { Link } from "react-router-dom";
-import { style } from "@mui/system";
-import styled from "styled-components";
+import './datatable.scss'
+import { DataGrid } from '@mui/x-data-grid'
+import { userColumns, userRows } from '../../../dataTableSource'
+import { Link } from 'react-router-dom'
+import { style } from '@mui/system'
+import styled from 'styled-components'
 
 const Datatable = () => {
   const actionColumn = [
     {
-      fied: "action",
-      headerName: "Action",
+      field: 'action',
+      headerName: 'Action',
       width: 60,
-      align: "center",
+      align: 'center',
       renderCell: () => {
         return (
           <div className="cellAction">
-            <Link to="/user/test" style={{ textDecoration: "none" }}>
+            <Link to="/user/test" style={{ textDecoration: 'none' }}>
               <div className="viewButton">View</div>
             </Link>
           </div>
-        );
+        )
       },
     },
-  ];
+  ]
   return (
     <div className="template">
       <div className="datatable">
         <Tab
           rows={userRows}
           columns={userColumns.concat(actionColumn)}
-          pageSize={5}
+          pageSize={10}
           rowsPerPageOptions={[5]}
           style={{
-            backgroundColor: "white",
-            fontSize: "1.4rem",
+            backgroundColor: 'white',
+            fontSize: '1.4rem',
           }}
         />
       </div>
     </div>
-  );
-};
+  )
+}
 
 const Tab = styled(DataGrid)({
-  "& .css-levciy-MuiTablePagination-displayedRows": {
-    fontSize: "1.4rem",
+  '& .css-levciy-MuiTablePagination-displayedRows': {
+    fontSize: '1.4rem',
   },
-});
+})
 
-export default Datatable;
+export default Datatable
--- src/features/logSystem/Component.jsx
+++ src/features/logSystem/Component.jsx
@@ -1,17 +1,104 @@
-import "./logsStyle.scss"
-import Navbar from '../../components/atoms/navbar/Navbar';
-import Sidebar from '../../components/atoms/sidebar/Sidebar';
+import './logsStyle.scss'
+import Navbar from '../../components/atoms/navbar/Navbar'
+import Sidebar from '../../components/atoms/sidebar/Sidebar'
+import ContentBox from '../../components/atoms/ContentBox'
+import { getLogs } from '../../apis/logsApi'
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { DataGrid } from '@mui/x-data-grid'
 
 const LogSystemComponent = () => {
+  const [logs, setLogs] = useState([])
+
+  useEffect(() => {
+    const handleGetLogs = async () => {
+      const resp = await getLogs()
+      const list = resp?.data?.data
+      setLogs(list)
+      console.log('🚀 ~ file: Component.jsx ~ line 20 ~ handleGetLogs ~ list', list)
+    }
+    handleGetLogs()
+  }, [])
+
+  const logsHeader = [
+    {
+      field: 'stt',
+      headerName: 'No',
+      width: 100,
+      align: 'center',
+      headerAlign: 'center',
+    },
+    {
+      field: 'id',
+      headerName: 'User ID',
+      width: 150,
+      align: 'center',
+      headerAlign: 'center',
+    },
+    {
+      field: 'date',
+      headerName: 'Date',
+      width: 250,
+      align: 'center',
+      headerAlign: 'center',
+    },
+    {
+      field: 'logType',
+      headerName: 'Logs type',
+      width: 200,
+      align: 'center',
+      headerAlign: 'center',
+    },
+    {
+      field: 'content',
+      headerName: 'Content',
+      width: 500,
+      align: 'left',
+      headerAlign: 'center',
+    },
+  ]
+
+  const logsContent = logs.map((item, index) => {
+    return {
+      stt: index + 1,
+      id: item.id,
+      date: item.date,
+      logType: item.logType,
+      content: item.content,
+    }
+  })
+
   return (
-    <div className='logs'>
+    <div className="logs">
       <Sidebar />
       <div className="logsContainer">
         <Navbar />
-        Logs component
+        <div className="body">
+          <ContentBox.Title title="Log system" />
+          <div className="template">
+            <div className="datatable">
+              <Tab
+                rows={logsContent}
+                columns={logsHeader}
+                pageSize={10}
+                rowsPerPageOptions={[10]}
+                style={{
+                  backgroundColor: 'white',
+                  fontSize: '1.4rem',
+                }}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
-export default LogSystemComponent;
+const Tab = styled(DataGrid)({
+  '& .css-levciy-MuiTablePagination-displayedRows': {
+    fontSize: '1.4rem',
+  },
+})
+
+export default LogSystemComponent
