d9186c7d68fb76f43e8b144cf82396052a82642f
unknown <tranthimyhuyen10@gmail.com>
2022-11-28 16:07:19 +0700

    statistic income

--- src/features/systemReport/Component.jsx
+++ src/features/systemReport/Component.jsx
@@ -1,21 +1,66 @@
 import './systemReportStyle.scss'
 import Navbar from '../../components/atoms/navbar/Navbar'
 import Sidebar from '../../components/atoms/sidebar/Sidebar'
-import { getStatistic } from '../../apis/statistic'
+import { getStatisticMonth, getStatisticQuarter } from '../../apis/statistic'
 import { useState } from 'react'
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
 
 const SystemReportComponent = () => {
   const [statistic, setStatistic] = useState()
+  const [income, setIncome] = useState()
   const [type, setType] = useState()
   const [year, setYear] = useState()
-  const [unit, setUnit] = useState()
+  const [month, setMonth] = useState()
+  const [quarter, setQuarter] = useState()
+  const [incomeType, setIncomeType] = useState()
+  const [incomYear, setIncomeYear] = useState()
+  const [incomeMonth, setIncomeMonth] = useState()
+  const [incomeQuarter, setIncomeQuarter] = useState()
 
-  const handleFilter = async () => {
+  const handleFilterMonth = async () => {
     try {
-      const resp = await getStatistic(unit, year, type)
+      const resp = await getStatisticMonth(month, year, type)
       const data = resp?.data?.data
       setStatistic(data)
+    } catch (error) {
+      return error
+    }
+  }
+
+  const handleFilterQuarter = async () => {
+    try {
+      const resp = await getStatisticQuarter(quarter, year, type)
+      const data = resp?.data?.data
+      setStatistic(data)
+    } catch (error) {
+      return error
+    }
+  }
+
+  const handleIncomeMonth = async () => {
+    try {
+      const resp = await getStatisticMonth(incomeMonth, incomYear, incomeType)
+      const data = resp?.data?.data
+      setIncome(data)
+    } catch (error) {
+      return error
+    }
+  }
+
+  const handleIncomeQuarter = async () => {
+    try {
+      const resp = await getStatisticQuarter(incomeQuarter, incomYear, incomeType)
+      const data = resp?.data?.data
+      setIncome(data)
     } catch (error) {
       return error
     }
@@ -27,8 +72,8 @@ const SystemReportComponent = () => {
       <div className="systemReportContainer">
         <Navbar />
         <div className="systemStatistic">
-          <div className="title">System statistic</div>
-          <div className="filter" style={{ display: 'flex', flexDirection: 'row' }}>
+          <div className="header">System statistic</div>
+          <div className="filter">
             <div className="form">
               <div className="title">Type</div>
               <select
@@ -39,7 +84,7 @@ const SystemReportComponent = () => {
                 }}
               >
                 <option value="MONTH">Month</option>
-                <option value="QUATER">Quater</option>
+                <option value="QUARTER">Quarter</option>
               </select>
             </div>
 
@@ -59,64 +104,194 @@ const SystemReportComponent = () => {
             </div>
 
             {type && type === 'MONTH' ? (
-              <div className="form">
-                <div className="title">Month</div>
-                <select
-                  className="select"
-                  id="unit"
-                  onChange={(e) => {
-                    setUnit(e.target.value)
-                  }}
-                >
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10</option>
-                  <option value="11">11</option>
-                  <option value="12">12</option>
-                </select>
-              </div>
-            ) : (
-              <div className="form">
-                <div className="title">Quater</div>
-                <select
-                  className="select"
-                  id="unit"
-                  onChange={(e) => {
-                    setUnit(Number(e.target.value))
-                  }}
-                >
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                </select>
-              </div>
-            )}
+              <>
+                <div className="form">
+                  <div className="title">Month</div>
+                  <select
+                    className="select"
+                    id="month"
+                    onChange={(e) => {
+                      setMonth(e.target.value)
+                    }}
+                  >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                  </select>
+                </div>
 
-            <button onClick={() => handleFilter(unit, year, type)}>Filter</button>
+                <button
+                  className="filterButton"
+                  onClick={() => handleFilterMonth(month, year, type)}
+                >
+                  Filter
+                </button>
+              </>
+            ) : (
+              <>
+                <div className="form">
+                  <div className="title">Quarter</div>
+                  <select
+                    className="select"
+                    id="quarter"
+                    onChange={(e) => {
+                      setQuarter(e.target.value)
+                    }}
+                  >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                  </select>
+                </div>
+                <button
+                  className="filterButton"
+                  onClick={() => handleFilterQuarter(quarter, year, type)}
+                >
+                  Filter
+                </button>
+              </>
+            )}
           </div>
           <div className="chartSystem">
-            <ResponsiveContainer width="100%" aspect={4}>
+            <ResponsiveContainer width="100%" aspect={3}>
               <LineChart
                 data={statistic}
                 width={500}
                 height={300}
-                margin={{ top: 20, right: 200, left: 20, bottom: 5 }}
+                margin={{ top: 20, right: 100, left: 20, bottom: 5 }}
               >
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="timeUnit" interval={'preserveStartEnd'} />
                 <YAxis />
+                <Tooltip
+                  contentStyle={{ backgroundColor: 'antiquewhite', borderRadius: '0.4rem' }}
+                />
                 <Legend />
-                <Line dataKey="nbuyer" stroke="red" />
-                <Line dataKey="nproduct" stroke="green" />
-                <Line dataKey="norder" stroke="blue" />
+                <Line type="monotone" dataKey="nbuyer" stroke="red" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="nproduct" stroke="green" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="norder" stroke="blue" activeDot={{ r: 8 }} />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+
+        <div className="revenueStatistics">
+          <div className="header">Revenue system</div>
+          <div className="filter">
+            <div className="form">
+              <div className="title">Type</div>
+              <select
+                className="select"
+                id="type"
+                onChange={(e) => {
+                  setIncomeType(e.target.value)
+                }}
+              >
+                <option value="MONTH">Month</option>
+                <option value="QUARTER">Quarter</option>
+              </select>
+            </div>
+
+            <div className="form">
+              <div className="title">Year</div>
+              <select
+                className="select"
+                id="year"
+                onChange={(e) => {
+                  setIncomeYear(e.target.value)
+                }}
+              >
+                <option value="2022">2022</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+              </select>
+            </div>
+
+            {incomeType && incomeType === 'MONTH' ? (
+              <>
+                <div className="form">
+                  <div className="title">Month</div>
+                  <select
+                    className="select"
+                    id="incomeMonth"
+                    onChange={(e) => {
+                      setIncomeMonth(e.target.value)
+                    }}
+                  >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                  </select>
+                </div>
+
+                <button
+                  className="filterButton"
+                  onClick={() => handleIncomeMonth(incomeMonth, incomYear, incomeType)}
+                >
+                  Filter
+                </button>
+              </>
+            ) : (
+              <>
+                <div className="form">
+                  <div className="title">Quarter</div>
+                  <select
+                    className="select"
+                    id="incomeQuarter"
+                    onChange={(e) => {
+                      setIncomeQuarter(e.target.value)
+                    }}
+                  >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                  </select>
+                </div>
+                <button
+                  className="filterButton"
+                  onClick={() => handleIncomeQuarter(incomeQuarter, incomYear, incomeType)}
+                >
+                  Filter
+                </button>
+              </>
+            )}
+          </div>
+          <div className="chartSystem">
+            <ResponsiveContainer width="100%" aspect={3}>
+              <LineChart
+                data={income}
+                width={500}
+                height={300}
+                margin={{ top: 20, right: 100, left: 20, bottom: 5 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="timeUnit" interval={'preserveStartEnd'} />
+                <YAxis />
+                <Tooltip
+                  contentStyle={{ backgroundColor: 'antiquewhite', borderRadius: '0.4rem' }}
+                />
+                <Legend />
+                <Line type="monotone" dataKey="income" stroke="orange" activeDot={{ r: 8 }} />
               </LineChart>
             </ResponsiveContainer>
           </div>
